8455c88a6386f4697ab47ac6d49566a61acc9eec
Jan Varwig <jan@varwig.org>
2018-05-12 11:58:07 +0200

    Move state handling out

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,99 +1,15 @@
 import * as React from 'react'
 import {Divider, Container} from 'rebass-emotion'
-import { createDay, Day, Slice as ISlice, TodoItem as ITodoItem, appendSlice } from './Data';
+import { createDay, Slice as ISlice, TodoItem as ITodoItem } from './Data';
 import Slice from './Slice'
 import TodoItem from './TodoItem'
-import { setIn, splice } from './util/icepick';
-
-export type FocusInfo = {type: 'slice', index:number, field:'title'|'time'} | {type: 'todoitem', index:number}
-
-interface State {
-  day: Day
-  todos: ITodoItem[]
-  focus: FocusInfo
-}
+import { handleFocusUp, handleFocusDown, handleFocusSlice, handleFocusTodo, handleCreateSliceFromTodo, handleDeleteSlice, handleChangeSlice, handleChangeTodoItem, State } from './State';
 
 export default class Main extends React.Component<{}, State> {
   state:State = {
     day: createDay(),
     todos: [{title: 'Standup'}, {title: 'Lunch'}],
     focus: {type: 'todoitem', index: 0}
-  }
-
-  handleFocusUp = () => {
-    const f = this.state.focus
-    if (f.type === 'slice' && f.index === 0) return;
-    if (f.type === 'todoitem' && f.index === 0) {
-      if (this.state.day.slices.length > 0) {
-        this.setState({focus: {type: 'slice', field:'title', index:this.state.day.slices.length-1}})
-        return
-      } else {
-        return
-      }
-    }
-    this.setState({
-      focus: {...f, index: f.index-1}
-    })
-  }
-
-  handleFocusDown = () => {
-    const f = this.state.focus
-    if (f.type === 'todoitem' && f.index === this.state.todos.length-1) {
-      if (this.getCurrentFocusItem().title == '') {
-        return
-      }
-      this.createNewTodo()
-    };
-    if (f.type === 'slice' && f.index === this.state.day.slices.length-1) {
-      // coming from the slices, want to go down into the todos
-      if (this.state.todos.length === 0) {
-        this.createNewTodo()
-      }
-      this.setState({focus: {type: 'todoitem', index:0}})
-      return
-    }
-    this.setState({
-      focus: {...f, index: f.index+1}
-    })
-  }
-
-  getCurrentFocusItem():ITodoItem|ISlice {
-    const f = this.state.focus
-    if (f.type === 'slice') {
-      return this.state.day.slices[f.index]
-    } else {
-      return this.state.todos[f.index]
-    }
-  }
-
-  handleFocusSlice = (slice: ISlice, field: 'time'|'title') => {
-    const f = this.state.focus
-    if (f.type === 'slice' && this.state.day.slices[f.index] === slice && f.field === field) {
-      return
-    }
-    this.setState({
-        focus: {
-          type: 'slice',
-          index: this.state.day.slices.indexOf(slice),
-          field
-        }
-    })
-  }
-
-  handleFocusTodo = (todo: ITodoItem) => {
-    const f = this.state.focus
-    if (f.type === 'todoitem' && this.state.todos[f.index] === todo) {
-      return
-    }
-    this.setState({
-      focus: {type: 'todoitem', index: this.state.todos.indexOf(todo)}
-    })
-  }
-
-  createNewTodo() {
-    this.setState({
-      todos: [...this.state.todos, {title: ''}]
-    })
   }
 
   render() {
@@ -129,38 +45,36 @@ export default class Main extends React.Component<{}, State> {
     )
   }
 
+  handleFocusUp = () => {
+    this.setState(handleFocusUp)
+  }
+
+  handleFocusDown = () => {
+    this.setState(handleFocusDown)
+  }
+
+  handleFocusSlice = (slice: ISlice, field: 'time'|'title') => {
+    this.setState(handleFocusSlice(slice, field))
+  }
+
+  handleFocusTodo = (todo: ITodoItem) => {
+    this.setState(handleFocusTodo(todo))
+  }
+
   handleCreateSliceFromTodo = (todo:ITodoItem) => {
-    let todos = this.state.todos.filter(t => t !== todo)
-    todos = todos.length > 0 ? todos : [{title: ''}]
-
-    let focus:FocusInfo = todos[this.state.focus.index]
-      ? this.state.focus
-      : {type: 'todoitem', index: todos.length-1}
-
-    this.setState({
-      day: appendSlice(this.state.day, todo.title),
-      todos,
-      focus
-    })
+    this.setState(handleCreateSliceFromTodo(todo))
   }
 
   handleChangeTodoItem = (todo: ITodoItem) => {
-    this.setState({
-      todos: setIn(this.state.todos, [this.state.focus.index], todo)
-    })
+    this.setState(handleChangeTodoItem(todo))
   }
 
   handleChangeSlice = (slice:ISlice) => {
-    this.setState({
-      day: setIn(this.state.day, ['slices', this.state.focus.index], slice)
-    })
+    this.setState(handleChangeSlice(slice))
   }
 
   handleDeleteSlice = (slice:ISlice) => {
-    const i = this.state.day.slices.indexOf(slice)
-    this.setState({
-      day: setIn(this.state.day, ['slices'], splice(this.state.day.slices, i, 1))
-    })
+    this.setState(handleDeleteSlice(slice))
   }
 
 }
---
+++ src/State.ts
@@ -0,0 +1,138 @@
+// import { setIn, splice } from './util/icepick';
+// import { createDay, Day, Slice as ISlice, TodoItem as ITodoItem, appendSlice } from './Data';
+import { Day, TodoItem as ITodoItem, TodoItem, Slice, appendSlice } from './Data';
+import { setIn, splice } from './util/icepick';
+
+export type FocusInfo = {type: 'slice', index:number, field:'title'|'time'} | {type: 'todoitem', index:number}
+
+export interface State {
+  day: Day
+  todos: ITodoItem[]
+  focus: FocusInfo
+}
+
+export function handleFocusUp(state:State) {
+  const f = state.focus
+  if (f.type === 'slice' && f.index === 0) return null;
+  if (f.type === 'todoitem' && f.index === 0) {
+    if (state.day.slices.length > 0) {
+      return ({
+        focus: {type: 'slice', field:'title', index:state.day.slices.length-1}
+      } as Pick<State, 'focus'>)
+    } else {
+      return null
+    }
+  }
+  return ({
+    focus: {...f, index: f.index-1}
+  } as Pick<State, 'focus'>)
+}
+
+export function handleFocusDown(state:State):Pick<State, never>|null {
+    const f = state.focus
+    if (f.type === 'todoitem' && f.index === state.todos.length-1) {
+      if (getCurrentFocusItem(state).title == '') {
+        return null
+      }
+      return createNewTodo(state)
+    };
+    if (f.type === 'slice' && f.index === state.day.slices.length-1) {
+      // coming from the slices, want to go down into the todos
+      let newState = {};
+      if (state.todos.length === 0) {
+        newState = createNewTodo(state)
+      }
+      return ({
+        ...newState,
+        focus: {type: 'todoitem', index:0}
+      })
+    }
+    return ({
+      focus: {...f, index: f.index+1},
+    })
+}
+
+function createNewTodo(state:State) {
+    return ({
+      todos: [...state.todos, {title: ''}]
+    })
+  }
+
+function getCurrentFocusItem(state:State):TodoItem|Slice {
+  const f = state.focus
+  if (f.type === 'slice') {
+    return state.day.slices[f.index]
+  } else {
+    return state.todos[f.index]
+  }
+}
+
+export function handleFocusSlice(slice: Slice, field: 'time'|'title') {
+  return (state:State) => {
+    const f = state.focus
+    if (f.type === 'slice' && state.day.slices[f.index] === slice && f.field === field) {
+      return null
+    }
+    return({
+        focus: {
+          type: 'slice',
+          index: state.day.slices.indexOf(slice),
+          field
+        }
+    } as Pick<State, 'focus'>)
+  }
+}
+
+export function handleFocusTodo(todo: ITodoItem) {
+  return (state:State) => {
+    const f = state.focus
+    if (f.type === 'todoitem' && state.todos[f.index] === todo) {
+      return null
+    }
+    return ({
+      focus: {type: 'todoitem', index: state.todos.indexOf(todo)}
+    } as Pick<State, 'focus'>)
+  }
+}
+
+export function handleCreateSliceFromTodo(todo:TodoItem) {
+  return (state:State) => {
+    let todos = state.todos.filter(t => t !== todo)
+    todos = todos.length > 0 ? todos : [{title: ''}]
+
+    let focus:FocusInfo = todos[state.focus.index]
+      ? state.focus
+      : {type: 'todoitem', index: todos.length-1}
+
+    return ({
+      day: appendSlice(state.day, todo.title),
+      todos,
+      focus
+    })
+  }
+}
+
+export function handleChangeTodoItem(todo: ITodoItem) {
+  return (state:State) => {
+    return ({
+      todos: setIn(state.todos, [state.focus.index], todo)
+    })
+  }
+}
+
+export function handleChangeSlice(slice:Slice) {
+  return (state:State) => {
+    return ({
+      day: setIn(state.day, ['slices', state.focus.index], slice)
+    })
+  }
+}
+
+export function handleDeleteSlice(slice:Slice) {
+  return (state:State) => {
+    const i = state.day.slices.indexOf(slice)
+    return ({
+      day: setIn(state.day, ['slices'], splice(state.day.slices, i, 1))
+    })
+  }
+}
