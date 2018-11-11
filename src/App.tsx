a1f370a758cdc88ba397c471acc183a233ef188f
Jan Varwig <jan@varwig.org>
2018-11-11 18:36:17 +0100

    Main style change

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,17 @@ import * as React from 'react';
 import Main from './Main'
 import StateContainer from './StateContainer'
 import 'modern-normalize'
+import { injectGlobal } from 'emotion';
+
+injectGlobal`
+  body, html {
+    height: 100%;
+  }
+
+  #root {
+    height: 100%;
+  }
+`
 
 class App extends React.Component {
   public render() {
